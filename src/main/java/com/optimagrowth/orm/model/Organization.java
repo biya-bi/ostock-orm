dff815d6f9b4534a5dad0f8087ac1d8c5f65293c
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-09-19 10:00:37 -0400

    Cascade organization remove operation

--- src/main/java/com/optimagrowth/orm/model/Organization.java
+++ src/main/java/com/optimagrowth/orm/model/Organization.java
@@ -3,6 +3,7 @@ package com.optimagrowth.orm.model;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -30,6 +31,6 @@ public class Organization {
     @Column(name = "contact_phone", nullable = false)
     private String contactPhone;
 
-    @OneToMany(mappedBy = "organization")
+    @OneToMany(mappedBy = "organization", cascade = CascadeType.REMOVE)
     private List<License> licenses;
 }
